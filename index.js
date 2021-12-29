94c5a06f302fb4360ca1715e999cb389ed6242ef
Christopher Terrano <mterrano1@yahoo.com>
2021-12-29 03:13:30 -0600

    Done.

--- index.js
+++ index.js
@@ -8,7 +8,50 @@ function myMap(collection, callback){
     return newArray
 }
 
-function myReduce(collection, callback, acc = 0){
-    let reducedValue = Object.values(collection).reduce(callback, acc);
+function myReduce(collection, callback, acc){
+    let newArray = Object.values(collection);
+    acc = acc || newArray.shift();
+    let reducedValue = newArray.reduce(callback, acc);
     return reducedValue
 }
+
+function myFind(collection, predicate){
+    return Object.values(collection).find(element => predicate(element))
+}
+
+function myFilter(collection, predicate){
+    return Object.values(collection).filter(element => predicate(element))
+}
+
+function mySize(collection){
+    return Object.values(collection).length
+}
+
+function myFirst(array, n){
+    let newArray = [];
+    if (n){
+        for (let i = 0; i < n; i++){
+            newArray.push(array[i])
+        }
+        return newArray
+    }
+    else 
+    {
+        return (array[0])
+    }
+}
+
+function myLast(array, n){
+    let newArray = [];
+    if (n){
+        for (let i = 0; i < n; i++){
+            newArray.push(array.pop())
+        }
+        return newArray
+    }
+    else 
+    {
+        return (array.pop())
+    }
+}
+
